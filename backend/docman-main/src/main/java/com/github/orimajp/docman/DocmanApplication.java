f6811d79e36411baba7b70678b39c9545e34c1c2
Hideo Oshima <orimajp@gmail.com>
2020-01-14 00:09:14 +0900

    バックエンド初期コミット

---
+++ backend/docman-main/src/main/java/com/github/orimajp/docman/DocmanApplication.java
@@ -0,0 +1,13 @@
+package com.github.orimajp.docman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocmanApplication.class, args);
+    }
+
+}
---
+++ backend/docman-main/src/test/java/com/github/orimajp/docman/DocmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.orimajp.docman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
