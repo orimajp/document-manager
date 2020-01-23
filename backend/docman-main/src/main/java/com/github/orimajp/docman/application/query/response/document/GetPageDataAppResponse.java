4abae40ac04d450f36de5ffa3fd682e67bf995cf
Hideo Oshima <orimajp@gmail.com>
2020-01-24 01:10:09 +0900

    ページデータアプリケーションレスポンスクラスをテスト実装

--- backend/docman-main/src/main/java/com/github/orimajp/docman/application/query/response/document/GetPageDataAppResponse.java
+++ backend/docman-main/src/main/java/com/github/orimajp/docman/application/query/response/document/GetPageDataAppResponse.java
@@ -1,28 +1,37 @@
 package com.github.orimajp.docman.application.query.response.document;
 
+import java.time.LocalDateTime;
+
 /**
  * ページデータ取得アプリケーションレスポンスクラス
  */
 public class GetPageDataAppResponse {
 
-    // ページキー
+    /** ページキー */
+    private String pageKey;
 
-    // ページタイトル
+    /** ページタイトル */
+    private String pageTitle;
 
-    // ページデータ
+    /** ページデータ */
+    private String pageData;
 
-    // 作成日
-    // 作成者ID
-    // 作成者名
+    /** 作成日時 */
+    private LocalDateTime createDateTime;
 
-    // 更新日時
-    // 更新者ID
-    // 更新者名
+    /** 作成者名 */
+    private String createUserName;
 
-    // ドキュメントリビジョン
+    /** 更新日時 */
+    private LocalDateTime updateDateTime;
 
-    // バージョン
+    /** 更新者名 */
+    private String updateUserName;
 
+    /** リビジョン */
+    private Integer revision;
 
+    /** バージョン */
+    private Integer version;
 
 }
